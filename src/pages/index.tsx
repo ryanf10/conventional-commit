82fa3ee2a6eccbd965659b48e8e8592b7f7ee0d2
Ryan Fernaldy <68326540+ryanf10@users.noreply.github.com>
2025-05-25 18:54:20 +0700

    feat: revamp button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { console.log("Login clicked") }}>
+        <button className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { console.log("Login clicked") }}>
           Login
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>
