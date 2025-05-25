9845452ddc656542a8f3524d6f4e4bfc07a9b28e
Ryan Fernaldy <68326540+ryanf10@users.noreply.github.com>
2025-05-25 18:57:33 +0700

    feat: remove carousel section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,16 +32,6 @@ export default function Home() {
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>
           Logout
         </button>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
