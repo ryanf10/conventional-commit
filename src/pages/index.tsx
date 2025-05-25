1a9dd812dab7640cb24d433a20833470d8611149
Ryan Fernaldy <68326540+ryanf10@users.noreply.github.com>
2025-05-25 18:47:06 +0700

    fix: crash on button click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>
           Button
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { console.log("Login clicked") }}>
           Login
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>
